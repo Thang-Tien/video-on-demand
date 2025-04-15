df9cd5aca6c5c0ae2af0ac4732ddef21d7b58cb8
Thang-Tien <cao.hl.thang@gmail.com>
2025-04-15 20:14:56 +0700

    feat: add implementation of trending video retrieval with pagination and DynamoDB integration

---
+++ services/video-management/get-trending-video/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type DynamoDBClientMock struct {
+	mock.Mock
+}
+
+func (m *DynamoDBClientMock) Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error) {
+	args := m.Called(ctx, params)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*dynamodb.QueryOutput), args.Error(1)
+}
+
+func TestGetTrendingVideo(t *testing.T) {
+	os.Setenv("EncryptionKey", "................................")
+	t.Run("should success get trending video", func(t *testing.T) {
+		dynamoDBClient := &DynamoDBClientMock{}
+		handler := &Handler{
+			DynamoDBClient: dynamoDBClient,
+		}
+
+		event := events.APIGatewayProxyRequest{}
+
+		dynamoDBClient.On("Query", mock.Anything, mock.Anything).Return(&dynamodb.QueryOutput{
+			Items: []map[string]types.AttributeValue{
+				{
+					"PK": &types.AttributeValueMemberS{Value: "VIDEO#123"},
+					"SK": &types.AttributeValueMemberS{Value: "METADATA"},
+				},
+			},
+			LastEvaluatedKey: map[string]types.AttributeValue{
+				"PK": &types.AttributeValueMemberS{Value: "VIDEO#123"},
+				"SK": &types.AttributeValueMemberS{Value: "METADATA"},
+			},
+		}, nil)
+
+		res, err := handler.HandleRequest(context.Background(), event)
+		if err != nil {
+			t.Errorf("expect no error, got %v", err)
+		}
+		assert.Equal(t, 200, res.StatusCode)
+		log.Printf("Response: %s", res.Body)
+	})
+}
